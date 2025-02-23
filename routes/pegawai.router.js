371528f9e414740cd054e713a1478ef15f4965bc
daffaagustians <145314938+daffaagustians@users.noreply.github.com>
2025-02-23 12:24:33 +0700

    done add tugas

--- routes/pegawai.router.js
+++ routes/pegawai.router.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var { auth } = require('../middlewares/auth.middleware');
 var {createTugas,editTugas, deleteTugas, getAllTugas} = require('../controllers/modul2/managementTugas.controller')
-var {getAllBiodata} = require('../controllers/modul3/managementBiodata');
+var {getAllBiodata, listPesertaAktif} = require('../controllers/modul3/managementBiodata');
 const { getPegawaiNotifications } = require('../controllers/modul1/statusRegistrasiKelompok');
 const { getFotoPegawai, getProfile, addProfilePhotobyPegawai } = require('../controllers/modul3/biodataPeserta.controller');
 const { upload } = require('../middlewares/foto.middleware');
@@ -20,7 +20,7 @@ router.get('/list-tugas', auth(['Pegawai']), getAllTugas);
 router.post('/add-tugas/:pesertaId', auth(['Pegawai']),createTugas)
 router.put('/edit-tugas/:tugasId', auth(['Pegawai']),editTugas)
 router.delete('/delete-tugas/:tugasId', auth(['Pegawai']), deleteTugas);
-router.get('/list-peserta',auth(['Pegawai']), getAllBiodata)
+router.get('/list-peserta',auth(['Pegawai']), listPesertaAktif)
 router.get('/list-notif', auth(['Pegawai']), getPegawaiNotifications);
 
 module.exports = router;
