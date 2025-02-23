4295ace42a7b2798e4679d3ac10b0e8641551608
daffaagustians <145314938+daffaagustians@users.noreply.github.com>
2025-02-23 22:51:22 +0700

    bismillah

--- controllers/modul3/sertifikatManagement.js
+++ controllers/modul3/sertifikatManagement.js
@@ -2,10 +2,7 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 const { transporter, EMAIL_USER } = require('../../middlewares/transporter.middleware');
 const path = require('path');
-const mammoth = require('mammoth');
-const fs = require('fs').promises; // Pakai promises langsung dari fs
 // Di bagian atas file, setelah require
-const libre = require('libreoffice-convert');
 const util = require('util');
 libre.convertAsync = util.promisify(libre.convert);
 //gabung
